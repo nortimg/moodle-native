a7dd23132dc684a27801ef3992f79e2d61dc98f6
MrEnderNasha <mc.mine.production@gmail.com>
2021-02-12 10:29:44 +0700

    Merged with AppBar

---
+++ src/navigation/ScreensObjectData.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {TouchableOpacity} from "react-native";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native'
+
+export const ScreensObjectData = (route) => {
+    const navigation = useNavigation();
+    const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+    const ScreensData = {
+        Home: {
+            headerTitle: 'ОГБПОУ "ТОМСКИЙ ТЕХНИКУМ ИНФОРМАЦИОННЫХ ТЕХНОЛОГИЙ"',
+            headerLeft: () => (
+                <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+                    <MaterialCommunityIcons name="menu" color={'#fff'} size={30} style={{
+                        marginLeft: 15
+                    }}/>
+                </TouchableOpacity>
+            ),
+            headerRight: () => (
+                <TouchableOpacity onPress={() => console.log('fired')}>
+                    <MaterialCommunityIcons name="magnify" color={'#fff'} size={30} style={{
+                        marginRight: 15
+                    }}/>
+                </TouchableOpacity>
+            ),
+        },
+        Calendar: {
+            headerTitle: 'Календарь',
+        },
+        Notification: {
+            headerTitle: 'Уведомление',
+        },
+        Message: {
+            headerTitle: 'Сообщения',
+        },
+        Files: {
+            headerTitle: 'Файлы'
+        }
+    }
+    return ScreensData[routeName];
+}
